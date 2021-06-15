01623a8a7aad219e37fa5cdf9a99285632ed2c7e
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-16 05:22:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,51 +6,16 @@ const Body = Matter.Body;
 let engine;
 let world;
 
-var ground;
+var cannon;
 
-var top_wall;
-var ball;
-var con;
-var btn1;
-var btn2;
 function setup() {
   createCanvas(400,400);
 
   engine = Engine.create();
   world = engine.world;
-  
-   var ball_options = {
-    restitution: 0.95,
-  }
-   
-  
-  btn2 = createImg('up.png');
-  btn2.position(20,30);
-  btn2.size(50,50);
-  btn2.mouseClicked(vForce);
-  
-   
-  
-  
 
-  ground =new Ground(200,390,400,20);
+    cannon = new Cannon(180, 110, 110, 50, angle);
 
-
-  ball = Bodies.circle(100,200,20,ball_options);
-  World.add(world,ball);
-  
-  
-  con = Matter.Constraint.create({
-    pointA:{x:200,y:20},
-    bodyB:ball,
-    pointB:{x:0,y:0},
-    length:100,
-    stiffness:0.1
-  });
-
-World.add(world,con);
-
-  
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -60,19 +25,8 @@ function draw()
 {
   background(51);
   Engine.update(engine);
-  
-  
+    cannon.display();
 
-  ellipse(ball.position.x,ball.position.y,20);
-  ground.show();
-  strokeWeight(2);
-  stroke(255);
-  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
-  Engine.update(engine);
+ 
 }
 
-
-function vForce()
-{
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});}
-  
