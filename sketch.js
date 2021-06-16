104c81083e6065fa8e6032f1b7ecd82398be7a3b
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-16 05:30:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-  angle = -PI / 4;
 
 let engine;
 let world;
@@ -14,6 +13,7 @@ function setup() {
 
   engine = Engine.create();
   world = engine.world;
+  angle = -PI / 4;
 
     cannon = new Cannon(180, 110, 110, 50, angle);
 
