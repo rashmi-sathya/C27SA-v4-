c9be6f7703c1d6e281cc6c85765a481dad5169a7
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-06-16 10:18:30 +0530

    Update Tower.js

--- js/Tower.js
+++ js/Tower.js
@@ -3,7 +3,6 @@ class Tower {
     var options = {
       isStatic: true
     };
-    this.image = loadImage("assets/tower.png");
     this.width = width;
     this.height = height;
     this.body = Bodies.rectangle(x, y, this.width, this.height, options);
@@ -11,12 +10,10 @@ class Tower {
   }
   display() {
     var pos = this.body.position;
-    var angle = this.body.angle;
     push();
     translate(pos.x, pos.y);
-    rotate(angle);
-    imageMode(CENTER);
-    image(this.image, 0, 0, this.width, this.height);
+     rectMode(CENTER);
+    rect( 0, 0, this.width, this.height);
     pop();
   }
 }
